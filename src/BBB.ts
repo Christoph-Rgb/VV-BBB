ce2df910aa2300ef8c2cb3247467f04505989898
Christoph-Rgb <chris.schwaegerl@gmail.com>
2018-11-08 21:39:25 +0100

    reading/writing json file now

--- src/BBB.ts
+++ src/BBB.ts
@@ -1,6 +1,6 @@
-import {Ticket} from './Ticket'
 import {Route} from './Route'
 import {IBBBCommand, RegisterRouteCommand} from './IBBBCommand'
+import * as fs from 'fs'
 
 const filePath = './.bbb_data'
 
@@ -26,16 +26,23 @@ export class BBB {
         const routeObjects = this._routes.map((r) => r.toObject())
         const json = JSON.stringify(routeObjects)
     
-        //TODO: save json to filePath
+        fs.writeFileSync(filePath, json)
     }
 
     private loadRoutes = () => {
         this._routes = new Array()
-        //TODO: load routesObjects
-        //for each routeObject call Routes.fromObject and push to this._routes
+
+        if (fs.existsSync(filePath)) {
+            const input = fs.readFileSync(filePath)
+            const routeObjects: Array<any> = JSON.parse(input.toString())
+    
+            for (const index in routeObjects) {
+                this._routes.push(Route.fromObject(routeObjects[index]))
+            }
+        }
     }
 
-    private parseCommand() {
+    private parseCommand = () => {
         
         let args = process.argv
         args.shift()
@@ -61,41 +68,3 @@ export class BBB {
 }
 
 const bbb = new BBB()
-
-
-// const ticket1 = new Ticket("Ticket1", 1)
-// console.log(ticket1.id)
-
-// const ticket2 = Ticket.fromJSON('{"id":"Ticket2", "seat":2, "boarded":true}')
-// console.log(ticket2.id)
-
-// const ticket3 = new Ticket("Ticket3", 3)
-// const json = ticket3.toJSON()
-// console.log(json)
-
-// const ticket3Clone = Ticket.fromJSON(json)
-// console.log(ticket3Clone.id + "Clone")
-
-// let route1 = new Route('R1', 'Madrid', 'Valencia', 50)
-
-// const ticket = route1.purchaseTicket().ticket
-// const ticket2 = route1.purchaseTicket().ticket
-// const ticket3 = route1.purchaseTicket().ticket
-
-// let result1 = route1.boardTicket(ticket2.id)
-// let result2 = route1.boardTicket('ticket2.id')
-
-// let result3 = route1.cancelTicket(ticket2.id)
-// let result4 = route1.cancelTicket('asldfkj')
-
-// const departed = route1.depart()
-// const hasArrived = route1.hasArrived()
-
-// const routeObject = route1.toObject()
-// const routeJSON = JSON.stringify(routeObject)
-
-// const recoveredRotueObject = JSON.parse(routeJSON)
-// const route1Clone = Route.fromObject(recoveredRotueObject)
-
-// console.log("")
-
